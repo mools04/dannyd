e262ab9ddb33ba3bb935fb3e1fea4ed2d0cf6330
mools04 <56056790+mools04@users.noreply.github.com>
2019-10-12 13:51:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+var i = 0;      // Start Point
+var images = [];  // Images Array
+var time = 3000;  // Time Between Switch
+   
+// Image List
+images[0] = "danny1.jpg";
+images[1] = "danny2.jpg";
+images[2] = "danny3.jpg";
+images[3] = "danny4.jpg";
+
+// Change Image
+function changeImg(){
+  document.slide.src = images[i];
+
+  // Check If Index Is Under Max
+  if(i < images.length - 1){
+    // Add 1 to Index
+    i++; 
+  } else { 
+    // Reset Back To O
+    i = 0;
+  }
+
+  // Run function every x seconds
+  setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
+
 function button(){
 	alert("Because he's hot AF");
 }
